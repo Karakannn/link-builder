a82d3cfb65527966c71f46c9b75fc96d534e1454
Karakannn <eminkarakan0@gmail.com>
2025-06-04 07:26:03 +0300

    test

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+
+const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/builder(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  try {
+    const session = await auth()
+
+    console.log("SESSION:", session)  
+    const { userId } = session || {}
+
+    console.log("userId", userId)
+    console.log("isProtectedRoute(req)", isProtectedRoute(req))
+
+    if (!userId && isProtectedRoute(req)) {
+      const customSignInUrl = new URL('/sign-in', req.url)
+      return NextResponse.redirect(customSignInUrl)
+    }
+
+    return NextResponse.next()
+  } catch (err) {
+    console.error("Middleware error:", err)
+    return NextResponse.next()
+  }
+})
+
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
